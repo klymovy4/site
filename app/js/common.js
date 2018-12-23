944c487d068d55771bc5d3f4fda5981761c861ea
klymovy4 <klymovy4roman@gmail.com>
2018-12-23 16:53:52 +0200

    well done

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,14 @@
 $(function() {
 
-  
+  $('#btn').click(function(){
+    // alert(123);
+    $(this).toggleClass('background');
+  });
+
+  // $('.tabs-buttons button').click(function(){
+    // alert(123);
+    // if((this).target.addClass('active')){
+      // $('p').parents.addClass('active');
+    // }
+  // });
 });
